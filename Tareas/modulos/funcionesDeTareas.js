66a15fe7ddd5d526975e94c694f5ad64dba9a539
PabloValles <valles.pablo@hotmail.com>
2022-02-15 15:15:56 -0300

    Cambios en funcionesDeTareas.js Agregamos el forEach para listar las tareas

--- Tareas/modulos/funcionesDeTareas.js
+++ Tareas/modulos/funcionesDeTareas.js
@@ -2,6 +2,12 @@ let fs = require("fs");
 let data = fs.readFileSync("./tareas.json");
 let tareas = JSON.parse(data);
 
-const listarTareas = (comando) => tareas;
+const listarTareas = (comando) => {
+  tareas.forEach((tarea, index, tareas) => {
+    console.log(`${index + 1}) - ${tarea.titulo}`);
+  });
+
+  // Retorna undefined
+};
 
 module.exports = listarTareas;
